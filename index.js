d9c936ddaa39fc686188586f7307d6c35727910e
sam-striker <samuelmupagasi@gamil.com>
2022-01-26 17:00:26 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/acmedb", { useNewUrlParser: true })
+  .then(() => {
+    const app = express();
+    app.use(express.json());
+    app.use("/api", routes);
+    console.log('MongoDB connected...')
+
+    app.listen(5000, () => {
+      console.log("Server has started!");
+    });
+  });
+
